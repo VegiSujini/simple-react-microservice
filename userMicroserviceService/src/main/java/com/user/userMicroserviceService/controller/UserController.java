5c18773aca5b53a1234bda3625fa73fd6e9efcd9
VegiSujini <sujini.vegi@gmail.com>
2024-09-21 13:31:18 +0530

    fixed the user issue

--- userMicroserviceService/src/main/java/com/user/userMicroserviceService/controller/UserController.java
+++ userMicroserviceService/src/main/java/com/user/userMicroserviceService/controller/UserController.java
@@ -3,6 +3,7 @@ package com.user.userMicroserviceService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping
     public List<User> getUsers() {
         return userService.getAllUsers();
     }
